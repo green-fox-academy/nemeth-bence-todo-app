9af2237f505825de8cbe10c92279f0a5b02d5f67
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-06 16:17:29 +0200

    To do app - add to list

--- to_do.py
+++ to_do.py
@@ -1,28 +1,33 @@
 import sys
 
-class Main:
-
-    def arg_reader():
-    	if len(sys.argv) == 1:
-            print_help()
-        else:
-    		if sys.argv[1] == '-l':
-                my_todo()
-
-    def print_help():
-        help_text = open('help.txt' , 'r')
-        help_text = help_text.read
-        print (help_text)
-
-    def open_my_todo():
-        my_todo = open('tasks.txt' , 'r')
-        my_todo = my_todo.read
-        print (my_todo)
-
-    arguments = arg_reader()
-
-'''    if len(arguments) == 0:
-    	print('help text')
+def arg_reader():
+	if len(sys.argv) == 1:
+        print_help()
     else:
-    	if( arguments[0] == '-l' ):
-    		print('Addolunk ilyet', arguments[1])'''
+        if sys.argv[1] == '-l':
+            open_todo()
+        if sys.argv[1] == '-a':
+            open_add()
+        if sys.argv[1] == '-r':
+            open_remove()
+        if sys.argv[1] == '-c':
+            open_complete()
+
+def print_help():
+    help_text = open('help.txt' , 'r')
+    help_text = help_text.read
+    print (help_text)
+
+def open_todo():
+    my_todo = open('tasks.txt' , 'r')
+    my_todo = my_todo.read
+    print (my_todo)
+
+def open_add()
+    add = open('tasks.txt' , 'a')
+    add = add.readlines
+    add.write("\n0;" + sys.argv[2].rstrip())
+
+
+
+arg_reader()
